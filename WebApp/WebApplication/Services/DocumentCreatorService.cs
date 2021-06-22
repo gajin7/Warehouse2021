51bb77f0eb0a7a670429d48b96e79e3617c9f9b3
gajin7 <gajinjovan@gmail.com>
2021-06-22 22:03:10 +0200

    Pdf maker refactoring

--- WebApp/WebApplication/Services/DocumentCreatorService.cs
+++ WebApp/WebApplication/Services/DocumentCreatorService.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Syncfusion.Pdf;
+using System;
 using System.Collections.Generic;
-using PdfSharp.Drawing;
-using PdfSharp.Pdf;
+using System.Drawing;
+using Syncfusion.Pdf.Graphics;
 using WebApplication.Models;
 using WebApplication.Repositories;
+using Syncfusion.Pdf.Grid;
+using System.Data;
 
 namespace WebApplication.Services
 {
@@ -18,47 +21,78 @@ namespace WebApplication.Services
         {
             try
             {
-                var pdf = new PdfDocument();
-                var pdfPage = pdf.AddPage();
-                var graph = XGraphics.FromPdfPage(pdfPage);
-                var font = new XFont("Verdana", 14, XFontStyle.Regular);
-                const int x = 20;
-                var y = 100;
-                var dy = (int)font.GetHeight() * 1; //change this factor to control line spacing
+                var path = AppDomain.CurrentDomain.BaseDirectory + @"\Documents\Receipts\Receipt" + receipt.Id + ".pdf";
+                PdfDocument doc = new PdfDocument();
 
+                //Add a page.
 
-                graph.DrawString("Items", font, XBrushes.Black,
-                    new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                y += dy;
-                graph.DrawString("Name      Quantity     Type     Amount", font, XBrushes.Black,
-                    new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                y += dy;
+                PdfPage page = doc.Pages.Add();
+
+                //Create a PdfGrid.
+
+                PdfGrid pdfGrid = new PdfGrid
+                {
+                    Style = new PdfGridStyle()
+                    {
+                        Font = new PdfStandardFont(PdfFontFamily.Helvetica,12),
+                    }
+                };
+
+                PdfGraphics graphics = page.Graphics;
+                graphics.DrawString("Receipt", new PdfStandardFont(PdfFontFamily.Helvetica, 36),
+                    PdfBrushes.Black, PointF.Empty);
+                graphics.DrawString("ID: " + receipt.Id, new PdfStandardFont(PdfFontFamily.Helvetica, 16),
+                    PdfBrushes.Black, new PointF(0, 50));
+                graphics.DrawString("Company: " + receipt.Company, new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 100));
+                graphics.DrawString("Date: " + receipt.Date, new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 150));
+                graphics.DrawString("Amount: " + receipt.Amount + " $",
+                    new PdfStandardFont(PdfFontFamily.Helvetica, 36),
+                    PdfBrushes.Black, new PointF(250, 200));
+
+                graphics.DrawString("Items ", new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 250));
+
+                //Create a DataTable.
+
+                DataTable dataTable = new DataTable();
+
+                dataTable.Columns.Add("Name");
+                dataTable.Columns.Add("Quantity");
+                dataTable.Columns.Add("Type");
+                dataTable.Columns.Add("Amount per item");
+                dataTable.Columns.Add("Total amount");
+
+                //Add rows to the DataTable.
+
                 foreach (var item in receiptItems)
                 {
-                    var itemValue = _itemRepository.GetItem(item.ItemId);
-                    var value = itemValue?.Name + " " + item.Quantity + " " + itemValue?.Type + " " +
-                              itemValue?.Amount * item.Quantity;
 
-                    graph.DrawString(value, font, XBrushes.Black,
-                        new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                    y += dy;
+                    var itemValue = _itemRepository.GetItem(item.ItemId);
+                    dataTable.Rows.Add(new object[]
+                    {
+                        itemValue?.Name, item.Quantity, itemValue?.Type, itemValue?.Amount + "$",
+                        itemValue?.Amount * item.Quantity  + "$"
+                    });
                 }
 
-                graph.DrawString("Receipt: " + receipt.Id, new XFont("Verdana", 20, XFontStyle.Bold), XBrushes.Black,
-                    new XRect(25, 25, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
 
-                graph.DrawString("Company: " + receipt.Company, new XFont("Verdana", 16, XFontStyle.Regular), XBrushes.Black,
-                    new XRect(25, 80, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
-                graph.DrawLine(new XPen(XColor.FromName("black")),new XPoint(),new XPoint());
-                graph.DrawString(receipt.Date, new XFont("Verdana", 16, XFontStyle.Regular), XBrushes.Black,
-                    new XRect(25, 150, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+                //Assign data source.
 
-                graph.DrawString("Amount: " + receipt.Amount, new XFont("Verdana", 20, XFontStyle.Bold), XBrushes.Black,
-                    new XRect(-10, -10, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.BottomRight);
+                pdfGrid.DataSource = dataTable;
 
-                var path = AppDomain.CurrentDomain.BaseDirectory + @"\Documents\Receipts\Receipt" + receipt.Id + ".pdf";
-                pdf.Save(path);
+                //Draw grid to the page of PDF document.
 
+                pdfGrid.Draw(page, new PointF(0, 290));
+
+                //Save the document.
+
+                doc.Save(path);
+
+                //close the document
+
+                doc.Close(true);
 
                 return new OperationResult
                 {
@@ -82,43 +116,76 @@ namespace WebApplication.Services
         {
             try
             {
-                var pdf = new PdfDocument();
-                var pdfPage = pdf.AddPage();
-                var graph = XGraphics.FromPdfPage(pdfPage);
-                var font = new XFont("Verdana", 14, XFontStyle.Regular);
-                const int x = 20;
-                var y = 100;
-                var dy = (int)font.GetHeight() * 1; //change this factor to control line spacing
+                var path = AppDomain.CurrentDomain.BaseDirectory + @"\Documents\Reports\Report" + report.Id + ".pdf";
 
+                PdfDocument doc = new PdfDocument();
 
-                graph.DrawString("Items", font, XBrushes.Black,
-                    new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                y += dy;
-                graph.DrawString("Name      Quantity     Type     Amount", font, XBrushes.Black,
-                    new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                y += dy;
+                //Add a page.
+
+                PdfPage page = doc.Pages.Add();
+
+                //Create a PdfGrid.
+
+                PdfGrid pdfGrid = new PdfGrid
+                {
+                    Style = new PdfGridStyle()
+                    {
+                        Font = new PdfStandardFont(PdfFontFamily.Helvetica, 12),
+                    }
+                };
+
+                PdfGraphics graphics = page.Graphics;
+                graphics.DrawString("Report", new PdfStandardFont(PdfFontFamily.Helvetica, 36),
+                    PdfBrushes.Black, PointF.Empty);
+                graphics.DrawString("ID: " + report.Id, new PdfStandardFont(PdfFontFamily.Helvetica, 16),
+                    PdfBrushes.Black, new PointF(0, 50));
+                graphics.DrawString("Type: " + report.Type, new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 100));
+                graphics.DrawString("Date: " + report.Date, new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 150));
+
+                graphics.DrawString("Items ", new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+                    PdfBrushes.Black, new PointF(0, 250));
+
+                //Create a DataTable.
+
+                DataTable dataTable = new DataTable();
+
+                dataTable.Columns.Add("Name");
+                dataTable.Columns.Add("Quantity");
+                dataTable.Columns.Add("Type");
+                dataTable.Columns.Add("Amount per item");
+                dataTable.Columns.Add("Total amount");
+
+                //Add rows to the DataTable.
+
                 foreach (var item in reportItems)
                 {
-                    var itemValue = _itemRepository.GetItem(item.ItemId);
-                    var value = itemValue?.Name + " " + item.Quantity + " " + itemValue?.Type + " " +
-                              itemValue?.Amount * item.Quantity;
 
-                    graph.DrawString(value, font, XBrushes.Black,
-                        new XRect(x, y, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
-                    y += dy;
+                    var itemValue = _itemRepository.GetItem(item.ItemId);
+                    dataTable.Rows.Add(new object[]
+                    {
+                        itemValue?.Name, item.Quantity, itemValue?.Type, itemValue?.Amount + "$",
+                        itemValue?.Amount * item.Quantity  + "$"
+                    });
                 }
 
-                graph.DrawString("Report: " + report.Id, new XFont("Verdana", 20, XFontStyle.Bold), XBrushes.Black,
-                    new XRect(25, 25, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
 
-                graph.DrawString("Type: " + report.Type, new XFont("Verdana", 16, XFontStyle.Regular), XBrushes.Black,
-                    new XRect(25, 80, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
-                graph.DrawLine(new XPen(XColor.FromName("black")), new XPoint(), new XPoint());
-                graph.DrawString(report.Date, new XFont("Verdana", 16, XFontStyle.Regular), XBrushes.Black,
-                    new XRect(25, 150, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+                //Assign data source.
 
-                var path = AppDomain.CurrentDomain.BaseDirectory + @"\Documents\Reports\Report" + report.Id + ".pdf";
-                pdf.Save(path);
+                pdfGrid.DataSource = dataTable;
+
+                //Draw grid to the page of PDF document.
+
+                pdfGrid.Draw(page, new PointF(0, 290));
+
+                //Save the document.
+
+                doc.Save(path);
+
+                //close the document
+
+                doc.Close(true);
 
 
                 return new OperationResult
@@ -136,7 +203,6 @@ namespace WebApplication.Services
                     ErrorMessage = e.Message
                 };
             }
-
         }
     }
 }
